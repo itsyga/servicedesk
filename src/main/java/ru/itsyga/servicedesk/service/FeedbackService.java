1b8333de0d823fe740d082657de113f4bd045b0c
itsyga <chel1997@mail.ru>
2024-10-24 00:10:43 +0500

    feat(service) add FeedbackService

---
+++ src/main/java/ru/itsyga/servicedesk/service/FeedbackService.java
@@ -0,0 +1,28 @@
+package ru.itsyga.servicedesk.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.itsyga.servicedesk.dto.FeedbackRequestPayload;
+import ru.itsyga.servicedesk.dto.FeedbackResponsePayload;
+import ru.itsyga.servicedesk.mapper.FeedbackMapper;
+import ru.itsyga.servicedesk.repository.FeedbackRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackService {
+    private final FeedbackMapper mapper;
+    private final FeedbackRepository feedbackRepository;
+
+    public List<FeedbackResponsePayload> findAll() {
+        return feedbackRepository.findAll().stream()
+                .map(mapper::toDto)
+                .toList();
+    }
+
+    public FeedbackResponsePayload save(FeedbackRequestPayload payload) {
+        var feedback = feedbackRepository.save(mapper.toEntity(payload));
+        return mapper.toDto(feedback);
+    }
+}
